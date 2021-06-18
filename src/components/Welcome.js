6c32fec5ad1011fdf736f5b79acfeece4bca94db
Dylan Koss <dylanhkoss@gmail.com>
2021-06-17 22:12:24 -0500

    first react commit

---
+++ src/components/Welcome.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+
+const Welcome = () => {
+
+    return (
+        <div className='card'>
+            <h1>Welcome to <span>CoreWeather</span></h1>
+            <h2>Give us your name and location</h2>
+            <form>
+            <div className='inputs'>
+            <input type='text' name='Name' placeholder='Name'></input>
+            <input type='text' name='Location' placeholder='Location'></input>
+            </div>
+            <button>Submit</button>
+            </form>
+        </div>
+    )
+}
+
+export default Welcome
